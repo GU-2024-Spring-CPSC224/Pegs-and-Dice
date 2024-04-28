f5d9bd826a92a96a66c451c5f96078de40f7c3e6
DavidSosa <davidgsosa1@gmail.com>
2024-04-27 19:28:15 -0700

    Testing commit

--- src/test/java/edu/gonzaga/MainGameTest.java
+++ src/test/java/edu/gonzaga/MainGameTest.java
@@ -1,27 +1,11 @@
 package edu.gonzaga;
 
-// import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainGameTest {
-    @Test
-    void alwaysTrue() {
+     @Test
+     void alwaysTrue() {
         Assertions.assertTrue(true);
-    }
-
-    @Test
-    void testHasWon() {
-        //Arrange
-        Player player = new Player();
-
-        //Act
-        for (int col = 0; col < 13; col++) {
-            player.getPlayerBoard()[0][col] = true;
-        }
-        
-        //Assert
-        Assertions.assertEquals(true, player.hasWon());
-    }
+     }
 }
